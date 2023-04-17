8e158f6e859030b41dde7b86666582f17eb45409
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-04-17 15:15:57 -0300

    :heavy_plus_sign: build: json-server, database and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      'dark-gray': '#333333',
+      'light-gray': '#EBEBEB',
+      'grayish-blue': '#646D82',
+      'light-grayish-blue': '#DEE2E6',
+      'green': '#1DB67B',
+      'light-green': '#DCF5EB',
+      'yellow': '#DCF5EB',
+      'red': '#F7617D'
+    },
+    fontFamily: {
+      lato: ['Lato', 'sans-serif'],
+      nunito: ['Nunito', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
 }
-
