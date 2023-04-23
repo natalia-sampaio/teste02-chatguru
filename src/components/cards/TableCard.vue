b120949496ed83911542aafb2cbca8bfc3f11782
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-04-23 15:00:26 -0300

    :lipstick: added little faces to the metrics table

--- src/components/cards/TableCard.vue
+++ src/components/cards/TableCard.vue
@@ -1,4 +1,7 @@
 <script setup>
+import IconSadFace from '../icons/IconSadFace.vue';
+import IconNeutralFace from '../icons/IconNeutralFace.vue';
+import IconHappyFace from '../icons/IconHappyFace.vue';
 import BaseTable from '../tables/BaseTable.vue';
 import BaseCard from './BaseCard.vue';
 import { getMetrics } from '@/services/npsRequests.js';
@@ -12,7 +15,7 @@ export default {
     data() {
         return {
             metrics: {}
-        }
+        };
     },
     async beforeMount() {
         this.metrics = await getMetrics();
@@ -32,7 +35,11 @@ export default {
             </template>
             <template #tableRows>
                 <tr v-for="score in scores" class="border-b border-light-grayish-blue font-bold" :class="score <= 6 ? 'text-red' : score <= 8 ? 'text-yellow' : 'text-green'">
-                    <td>#</td>
+                    <td>
+                        <IconSadFace class="mr-4" v-if="score <= 6" />
+                        <IconNeutralFace class="mr-4" v-else-if="score <= 8" />
+                        <IconHappyFace class="mr-4" v-else/>
+                    </td>
                     <td>{{ score }}</td>
                     <td>{{ metrics[`score_${score}`] }}</td>
                     <td>{{ (metrics[`score_${score}`]/metrics.total_answered * 100).toFixed(1) }}%</td>
